a922f6d216cfe615d63a3a57f8f8a771f1fec459
Sarah Kristian Perry <sarah.perry@keyin.com>
2025-08-02 15:37:07 -0230

    Add Registration module with entities, service, repository, and REST controller.
    Integrate `Registration` entity to manage tournament registrations. Establish relationships to `Member` and `Tournament` entities. Implement service logic, repository methods, and REST endpoints for CRUD operations and member-tournament interactions.

---
+++ src/main/java/com/teeparty/tournament/registration/Registration.java
@@ -0,0 +1,35 @@
+package com.teeparty.tournament.registration;
+
+import com.teeparty.tournament.member.Member;
+import com.teeparty.tournament.tournament.Tournament;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+
+@Entity
+public class Registration {
+    @Id
+    @SequenceGenerator(name = "registration_sequence", sequenceName = "registration_sequence, allocationSize = 1")
+    @GeneratedValue(generator = "registration_sequence", strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne
+    @JoinColumn(name = "tournament_id")
+    private Tournament tournament;
+
+    private LocalDateTime registrationDate;
+
+    public Registration() {
+    }
+
+}
